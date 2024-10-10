eb432587f1ff70051f9c17aac9a4af3c7ed22079
Olia <oliaopanasiukk@gmail.com>
2024-10-10 20:05:01 +0300

    add user and role managers setup

--- api/TaskTracker/TaskTracker.Web/Program.cs
+++ api/TaskTracker/TaskTracker.Web/Program.cs
@@ -39,6 +39,9 @@ if (app.Environment.IsDevelopment())
 using (IServiceScope scope = app.Services.CreateScope())
 {
     TaskTrackerDbContext dbContext = scope.ServiceProvider.GetRequiredService<TaskTrackerDbContext>();
+    UserManager<UserEntity> userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+    RoleManager<RoleEntity> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+
     dbContext.Database.Migrate();
 }
 
